9b148b4c6b8d8b718d5b81b32a04e3a7079a03a1
ATien0108 <129314834+ATien0108@users.noreply.github.com>
2023-11-30 09:10:31 +0700

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -6,24 +6,29 @@ class CartAdmin(admin.ModelAdmin):
     list_display = ('cart_id', 'customer_id', 'product_id_size', 'cart_quantity', 'cart_complete')
     search_fields = ['cart_id']
     list_filter = ('cart_id', 'customer_id', 'product_id_size', 'cart_quantity', 'cart_complete')
+    list_editable = ('customer_id', 'product_id_size', 'cart_quantity', 'cart_complete')
 admin.site.register(Cart, CartAdmin)
 
 class ShippingAdmin(admin.ModelAdmin):
     list_display = ('Shipping_id', 'order_id', 'first_name', 'last_name', 'shipping_email', 'shipping_address', 'shipping_phonenumber', 'shipping_description')
-    search_fields = ['emp_name']
+    search_fields = ['shipping_phonenumber']
     list_filter = ('Shipping_id', 'order_id', 'first_name', 'last_name', 'shipping_email', 'shipping_address', 'shipping_phonenumber', 'shipping_description')
+    list_editable = ('order_id', 'first_name', 'last_name', 'shipping_email', 'shipping_address', 'shipping_phonenumber', 'shipping_description')
 admin.site.register(Shipping, ShippingAdmin)
 
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('emp_id', 'emp_name', 'emp_age', 'emp_address', 'emp_phonenumber', 'emp_email', 'emp_position', 'Status_of_work')
     search_fields = ['emp_name']
     list_filter = ('emp_id', 'emp_name', 'emp_age', 'emp_address', 'emp_phonenumber', 'emp_email', 'emp_position', 'Status_of_work')
+    list_editable = ('emp_name', 'emp_age', 'emp_address', 'emp_phonenumber', 'emp_email', 'emp_position', 'Status_of_work')
 admin.site.register(Employee, EmployeeAdmin)
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_id_size', 'product_name', 'product_description', 'product_size', 'product_image', 'product_price', 'category_id', 'product_id')
     search_fields = ['product_name']
     list_filter = ('product_id_size', 'product_name', 'product_description', 'product_size', 'product_image', 'product_price', 'category_id', 'product_id')
+    list_editable = ('product_name', 'product_description', 'product_size', 'product_image', 'product_price', 'category_id', 'product_id')
+
 admin.site.register(Product, ProductAdmin)    
     
 class CategoryAdmin(admin.ModelAdmin):
@@ -36,6 +41,8 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ('customer_id', 'customer_name', 'customer_address', 'customer_phonenumber', 'customer_email')
     search_fields = ['customer_name']
     list_filter = ('customer_id', 'customer_name', 'customer_address', 'customer_phonenumber', 'customer_email')
+    list_editable = ('customer_name', 'customer_address', 'customer_phonenumber', 'customer_email')
+
 admin.site.register(Customer, CustomerAdmin)
 
 # class CustomerAccountAdmin(admin.ModelAdmin):
@@ -48,22 +55,22 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('order_id', 'customer_id', 'order_date', 'order_total')
     search_fields = ['order_id']
     list_filter =('order_id', 'customer_id', 'order_date', 'order_total')
+    list_editable =('customer_id', 'order_date', 'order_total')
+
 admin.site.register(Orders, OrderAdmin)
 
 class OrderDetailsAdmin(admin.ModelAdmin):
     list_display = ('order_detail_id', 'order_id', 'product_id_size', 'order_detail_quantity', 'order_detail_total')
     search_fields = ['order_detail_id']
     list_filter = ('order_detail_id', 'order_id', 'product_id_size', 'order_detail_quantity', 'order_detail_total')
-admin.site.register(OrderDetails, OrderDetailsAdmin)
+    list_editable = ('order_id', 'product_id_size', 'order_detail_quantity', 'order_detail_total')
 
-# class EmployeeAccountAdmin(admin.ModelAdmin):
-#     list_display = ('emp_acc_id', 'emp_id', 'emp_username', 'emp_password')
-#     search_fields = ['emp_acc_id']
-#     list_filter = ('emp_acc_id', 'emp_id', 'emp_username', 'emp_password')
-# admin.site.register(EmployeeAccount, EmployeeAccountAdmin)
+admin.site.register(OrderDetails, OrderDetailsAdmin)
 
 class RevenueAdmin(admin.ModelAdmin):
     list_display = ('revenue_id', 'revenue_month', 'revenue_Total')
     search_fields = ['revenue_id']
     list_filter = ('revenue_id', 'revenue_month', 'revenue_Total')
+    list_editable = ('revenue_month', 'revenue_Total')
+
 admin.site.register(Revenue, RevenueAdmin)
